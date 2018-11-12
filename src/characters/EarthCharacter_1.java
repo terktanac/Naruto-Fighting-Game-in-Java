55c76758328b89a2b5be383128020c17dcc77bad
Thammakorn Kobkuachaiyapong <chan_tham@hotmail.com>
2018-11-12 15:49:18 +0700

    Add Earth Character

---
+++ src/characters/EarthCharacter_1.java
@@ -0,0 +1,70 @@
+package characters;
+
+public class EarthCharacter_1 extends Character {
+
+	public EarthCharacter_1(String name, int element, int health, int atk, int def) {
+		super("Toph", 2, 100, 5, 10);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int melee() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int range() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int dodge() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int block() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int walk() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int crouch() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int dash() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int basic_skill() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int mid_skill() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int High_skill() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
