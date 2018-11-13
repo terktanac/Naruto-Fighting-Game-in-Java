f5250e514a0067572b535f3f7b12df3de176bdd8
Thammakorn Kobkuachaiyapong <chan_tham@hotmail.com>
2018-11-13 08:31:29 +0700

    New Plain Characters

---
+++ src/characters/PlainCharacter_1.java
@@ -0,0 +1,71 @@
+package characters;
+
+public class PlainCharacter_1 extends Character{
+
+	public PlainCharacter_1() {
+		super(name, element, health, atk, def, standTme);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int melee() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int range() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int dodge() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int block() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int walk() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int crouch() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int dash() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int basic_skill() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int mid_skill() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int High_skill() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+}
