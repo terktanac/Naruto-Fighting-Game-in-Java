f5e7b1216d6760ec8b53b197d84cd8e525feee3d
Thammakorn Kobkuachaiyapong <chan_tham@hotmail.com>
2018-11-16 18:17:21 +0700

    Add Event handling and Blink

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,35 +1,67 @@
 package main;
 
+import java.io.IOException;
+
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.geometry.Pos;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Main extends Application {
-	
-	private static final Font FONT = new Font("/CharsAndSFX/njnaruto.ttf", 25); //fix font
+//	private Font narutoFont = new Font(getClass().getResource("//CharsAndSFX//fonts//njnaruto.ttf").toExternalForm(), 50); fix pls
+	private Stage primaryStage;
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		this.primaryStage = primaryStage;
 		Pane root = new Pane();
+		MainmenuScreen NextScene = new MainmenuScreen();
 		root.setPrefSize(1280, 720);
-
-		Rectangle background = new Rectangle(1280,720);
+		Rectangle background = new Rectangle(1280, 720);
 		Text pressKey = new Text("Press any key to START");
-		pressKey.setFont(FONT);
+		pressKey.setFont(new Font(40));
 		pressKey.setFill(Color.WHITE);
-		pressKey.setTranslateX(440);
-		pressKey.setTranslateY(360);
-		root.getChildren().addAll(background,pressKey);
+		pressKey.setTranslateX(420);
+		pressKey.setTranslateY(500);
+		
+		root.getChildren().addAll(background, pressKey);
+		
+		Scene scene = new Scene(root);
+		
 		primaryStage.setTitle("Naruto Ultimate Ninja by C&T");
-		primaryStage.setScene(new Scene(root));
+		primaryStage.setScene(scene);
 		primaryStage.setResizable(false);
 		primaryStage.show();
+		//To Blink
+		Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0.2), evt -> pressKey.setFill(Color.YELLOW)),
+				new KeyFrame(Duration.seconds(0.1), evt -> pressKey.setFill(Color.WHITE)));
+		timeline.setCycleCount(Animation.INDEFINITE);
+		timeline.play();
+		//Change Scene
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				primaryStage.getScene().setRoot(NextScene);
+				System.out.println("PRESSED");
+			}
+		});
+	}
 
+	public void changeScene(String fxml) throws IOException {
+		Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+
+		primaryStage.getScene().setRoot(pane);
 	}
 
 	public static void main(String[] args) {
--- src/main/MainmenuScreen.java
+++ src/main/MainmenuScreen.java
@@ -1,5 +1,17 @@
 package main;
 
-public class MainmenuScreen {
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
+public class MainmenuScreen extends Pane {
+	public MainmenuScreen() {
+		setBackground(new Background(new BackgroundFill(Color.GREEN, null, null)));
+	}
+	public class Menu extends VBox{
+		
+	}
 }
