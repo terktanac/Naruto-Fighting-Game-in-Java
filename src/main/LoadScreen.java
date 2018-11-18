cb34a7dc75651661648081a39261f622c1545823
Tana Chanchiew <terktana18@gmail.com>
2018-11-18 16:37:46 +0700

    add change characters

--- src/main/LoadScreen.java
+++ src/main/LoadScreen.java
@@ -19,6 +19,7 @@ import javafx.scene.paint.Color;
 import main.MainMenuScreen.ListMenu;
 
 public class LoadScreen extends Scene{
+	private int player1default = 0,player2default = 1;
 	private static Pane root = new Pane();
 	private ImageView lhschar = new ImageView(new Image(ClassLoader.getSystemResource("characters/naruto_sage/naruto_sage_mugs.png").toString(), 400, 250, true, true));
 	private ImageView rhschar = new ImageView(new Image(ClassLoader.getSystemResource("characters/sasuke_aka/sasuke_aka_mugs.png").toString(), 400, 250, true, true));
@@ -26,7 +27,7 @@ public class LoadScreen extends Scene{
 		super(root);
 		ArrayList<String> myCharacters = new ArrayList<String>();
 		myCharacters.add("characters/naruto_sage/naruto_sage_mugs.png");
-		myCharacters.add("characters/sauke_aka/sauke_aka_mugs.png");
+		myCharacters.add("characters/sasuke_aka/sasuke_aka_mugs.png");
 		
 		root.setPrefSize(1280, 720);
 		root.setBackground(new Background(new BackgroundFill(Color.AQUA, null, null)));
@@ -46,17 +47,27 @@ public class LoadScreen extends Scene{
 				click.play();
 				KeyCode key = event.getCode();
 				System.out.println("Multiplayer:Pressed " + key.toString());
+				
+				
+				
 				if (key == KeyCode.SPACE || key == KeyCode.ENTER) {
 
-				}
-				else if (key == KeyCode.UP || key == KeyCode.LEFT || key == KeyCode.W || key == KeyCode.A || key == KeyCode.KP_UP || key == KeyCode.KP_LEFT) {
-						
 				} 
-				else if (key == KeyCode.UP || key == KeyCode.LEFT || key == KeyCode.W || key == KeyCode.A || key == KeyCode.KP_UP || key == KeyCode.KP_LEFT) {
-					
+				else if (key == KeyCode.W || key == KeyCode.A) {
+					player1default = (player1default+1+myCharacters.size())%myCharacters.size();
+					lhschar.setImage(new Image(ClassLoader.getSystemResource(myCharacters.get(player1default)).toString(), 400, 250, true, true));
 				}
-				else if (key == KeyCode.DOWN ||key == KeyCode.RIGHT || key == KeyCode.S || key == KeyCode.D || key == KeyCode.KP_DOWN || key == KeyCode.KP_RIGHT) {
-						
+				else if (key == KeyCode.UP || key == KeyCode.LEFT) {
+					player2default = (player2default+1+myCharacters.size())%myCharacters.size();
+					rhschar.setImage(new Image(ClassLoader.getSystemResource(myCharacters.get(player2default)).toString(), 400, 250, true, true));
+				}
+				else if (key == KeyCode.S || key == KeyCode.D) {
+					player1default = (player1default-1+myCharacters.size())%myCharacters.size();
+					lhschar.setImage(new Image(ClassLoader.getSystemResource(myCharacters.get(player1default)).toString(), 400, 250, true, true));
+				}
+				else if (key == KeyCode.DOWN ||key == KeyCode.RIGHT) {
+					player2default = (player2default-1+myCharacters.size())%myCharacters.size();
+					rhschar.setImage(new Image(ClassLoader.getSystemResource(myCharacters.get(player2default)).toString(), 400, 250, true, true));
 				}
 			}
 		});
