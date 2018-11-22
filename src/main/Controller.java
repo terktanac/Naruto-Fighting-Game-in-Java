0e9d8e82142d1a162a4be7dfc8f9c39e539ef0e4
Thammakorn Kobkuachaiyapong <chan_tham@hotmail.com>
2018-11-22 21:54:31 +0700

    controller

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,10 +1,19 @@
 package main;
 
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 public class Controller implements Runnable{
-	private KeyCode leftKey = KeyCode.LEFT;
-	private KeyCode rightKey = KeyCode.RIGHT;
+	private KeyCode upKey;
+	private KeyCode downKey;
+	private KeyCode leftKey ;
+	private KeyCode rightKey;
+	private KeyCode meleeKey ;
+	private KeyCode rangeKey;
+	private KeyCode blockKey;
+	private KeyCode dodgeKey ;
 	private double x_pos;
 	private double y_pos;
 	private double x_speed;
@@ -12,23 +21,121 @@ public class Controller implements Runnable{
 	private double x;
 	private double y;
 	private boolean isJumping = false;
-	@Override
-	public void run() {
-		/*if(moveLeft()) {
-			x_pos += x_speed ;
-		}
-		if(moveRight()) {
-			x_pos -= x_speed ;
-		}
-		if(jump()) {
-			isJumping = true;
-		}
-		if(isJumping) {
-			
-		}
-		if(crouch()) {
-			x /= 2;
-		}*/
+	private Scene scene;
+	
+	
+	public Controller(KeyCode upKey, KeyCode downKey, KeyCode leftKey, KeyCode rightKey, KeyCode meleeKey,
+			KeyCode rangeKey, KeyCode blockKey, KeyCode dodgeKey) {
+		super();
+		this.upKey = upKey;
+		this.downKey = downKey;
+		this.leftKey = leftKey;
+		this.rightKey = rightKey;
+		this.meleeKey = meleeKey;
+		this.rangeKey = rangeKey;
+		this.blockKey = blockKey;
+		this.dodgeKey = dodgeKey;
 	}
 
+
+	@Override
+	public void run() {
+		System.out.println("HI");
+	}
+
+//	public void keyHandling() {
+//		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+//			@Override
+//			public void handle(KeyEvent event) {
+//				KeyCode key = event.getCode();
+//				System.out.println("Controller: "+key);
+//				
+//			}
+//		});
+//	}
+	public void setScene(Scene scene) {
+		this.scene = scene;
+	}
+
+
+	public KeyCode getUpKey() {
+		return upKey;
+	}
+
+
+	public void setUpKey(KeyCode upKey) {
+		this.upKey = upKey;
+	}
+
+
+	public KeyCode getDownKey() {
+		return downKey;
+	}
+
+
+	public void setDownKey(KeyCode downKey) {
+		this.downKey = downKey;
+	}
+
+
+	public KeyCode getLeftKey() {
+		return leftKey;
+	}
+
+
+	public void setLeftKey(KeyCode leftKey) {
+		this.leftKey = leftKey;
+	}
+
+
+	public KeyCode getRightKey() {
+		return rightKey;
+	}
+
+
+	public void setRightKey(KeyCode rightKey) {
+		this.rightKey = rightKey;
+	}
+
+
+	public KeyCode getMeleeKey() {
+		return meleeKey;
+	}
+
+
+	public void setMeleeKey(KeyCode meleeKey) {
+		this.meleeKey = meleeKey;
+	}
+
+
+	public KeyCode getRangeKey() {
+		return rangeKey;
+	}
+
+
+	public void setRangeKey(KeyCode rangeKey) {
+		this.rangeKey = rangeKey;
+	}
+
+
+	public KeyCode getBlockKey() {
+		return blockKey;
+	}
+
+
+	public void setBlockKey(KeyCode blockKey) {
+		this.blockKey = blockKey;
+	}
+
+
+	public KeyCode getDodgeKey() {
+		return dodgeKey;
+	}
+
+
+	public void setDodgeKey(KeyCode dodgeKey) {
+		this.dodgeKey = dodgeKey;
+	}
+	
+	
 }
